aa9d754ca6dda15c12b1dd05c870cfce671ad59f
Diego Banovaz <diego@strive.school>
2019-10-28 08:40:25 +0100

    small fix

--- server.js
+++ server.js
@@ -19,10 +19,10 @@ server.use("/user", cors(), userRouter);
 console.log(listRoutes(server));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  server.use(express.static("client/build"));
 }
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+server.get("*", (req, res) => {
+  server.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
 mongoose
